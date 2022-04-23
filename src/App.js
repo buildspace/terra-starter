7e96aa5f50d9aa2c62f7a704b21ee97d07a39629
Raza <42661870+AlmostEfficient@users.noreply.github.com>
2022-04-23 16:01:28 +1200

    Prep for launch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import './App.css';
 
-import GameMenu from './components/GameMenu';
-import WalletAddress from './components/WalletAddress';
-
-import { ConnectWallet } from './components/ConnectWallet';
-import {
-  useWallet,
-  useConnectedWallet,
-  WalletStatus,
-} from '@terra-money/wallet-provider';
-
 function App() {
-  const { status } = useWallet();
-  const connectedWallet = useConnectedWallet();
 
-  return connectedWallet?.network?.name === "mainnet" ? (
-    <span>Please connect to the Bombay testnet!</span>
-  ) : (
+  return (
     <main className="App">
       <header>
         <div className="header-titles">
@@ -24,17 +10,14 @@ function App() {
           <p>Only you can save us from Goblin town</p>
         </div>
 
-        <WalletAddress />
       </header>
 
-        {status === WalletStatus.WALLET_CONNECTED && (
-          <div className="game-menu-container">
-            <GameMenu />
-          </div>
-        )}
-
-      <ConnectWallet />
-
+      <div>
+        <img
+          src="https://media.giphy.com/media/B19AYwNXoXtcs/giphy.gif"
+          alt="Goblin gif"
+        />
+      </div>
     </main>
   );
 }
--- src/contract/address.js
+++
@@ -1,4 +0,0 @@
-// sync-ed from root via `tr sync-refs`
-import config from "../refs.terrain.json"
-
-export const contractAddress = (wallet) => config[wallet.network.name].blank.contractAddresses.default
